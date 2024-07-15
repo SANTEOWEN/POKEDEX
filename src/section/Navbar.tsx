bee3bc21bf5ed1236af4a23d06766956a3265726
OWSNT <owensante45@gmail.com>
2024-07-15 15:34:37 +0800

    First Commit

---
+++ src/section/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import pokeballIcon from "../assets/pokeball-icon.png"
+import { GiHamburgerMenu } from 'react-icons/gi'
+function Navbar() {
+  return (
+    <nav>
+      <div className="block">
+        <img src={pokeballIcon} alt="Pokeball-icon" className='' />
+      </div>
+      <div className="data">
+
+      </div>
+      <div className="block">
+        <GiHamburgerMenu />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
